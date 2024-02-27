47b609f43a1e3e776ae0d96f7e70f6b46cfc60f4
Dyan Kastutara <dyankastutara19@gmail.com>
2024-02-27 17:00:35 +0700

    fixed daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#fa541c",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "dark",
+      "cupcake",
+    ],
+  },
 };
